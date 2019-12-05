c96a20ebd52c501737c283670d80b1fe96c790c2
shaik-rehana <rehaansk1001@gmail.com>
2019-12-05 18:54:40 +0530

    Added Seleniumjava

---
+++ RehanaTestingClass/src/com/kotak/loans/carloans/FirstClass.java
@@ -0,0 +1,11 @@
+package com.kotak.loans.carloans;
+
+public class FirstClass {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi ! ");
+		
+	}
+
+}
---
+++ RehanaTestingClass/src/com/kotak/loans/carloans/SecondClass.java
@@ -0,0 +1,11 @@
+package com.kotak.loans.carloans;
+
+public class SecondClass {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Renault Triber");
+		
+	}
+
+}
